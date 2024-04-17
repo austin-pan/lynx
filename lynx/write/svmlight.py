1ff7a640c9c01a67db871b8a26ef4bae96fa17bb
Austin Pan <22647854+austin-pan@users.noreply.github.com>
2024-04-16 22:28:01 -0700

    Add svmlight write

---
+++ lynx/write/svmlight.py
@@ -0,0 +1,22 @@
+from typing import Iterable
+from sklearn import datasets
+
+import lynx as lx
+
+
+def write_svmlight(
+    table: lx.Table,
+    target: Iterable[float],
+    path: str,
+) -> None:
+    """
+    Write provided Table with the provided target values in materialized form to
+    a single SVMLight file.
+
+    Args:
+        table (Table): Table to write.
+        target (Iterable[float]): Target values to use.
+        path (str): Output filepath.
+    """
+    data = table.to_csr_matrix()
+    datasets.dump_svmlight_file(X=data, y=target, f=path) # type: ignore
