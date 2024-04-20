f59512471d7d73e451d0c1d8689fc0c00c619f95
Austin Pan <22647854+austin-pan@users.noreply.github.com>
2024-04-20 06:33:05 -0700

    Set up netflix prize experiments

---
+++ experiments/netflixprize/movie_user_interactions/data_loader.py
@@ -0,0 +1,32 @@
+from typing import Tuple
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import lynx as lx
+from lynx.datasets import netflixprize
+
+
+DATASET_PATH = "~/Downloads/lynx/datasets/netflix/download"
+SEED = 0
+VERBOSE = False
+
+def load_train_test() -> Tuple[lx.Table, lx.Table, pd.Series, lx.Table, pd.Series]:
+    ratings_data = netflixprize.load_data(
+        DATASET_PATH,
+        nrows=10
+    )
+    netflix_table = (
+        lx.Table(ratings_data, "ratings")
+        .drop("date")
+        .model_interactions("movie_id", "user_id")
+        .onehot("movie_id")
+        .onehot("user_id")
+    )
+
+    y = netflix_table.pop("rating")
+    X_train, X_test, y_train, y_test = train_test_split(
+        netflix_table, y,
+        train_size=0.8,
+        test_size=0.2,
+        random_state=SEED
+    )
+    return netflix_table, X_train, y_train, X_test, y_test
--- lynx/datasets/netflixprize.py
+++ lynx/datasets/netflixprize.py
@@ -1,39 +1,61 @@
 """https://www.kaggle.com/datasets/netflix-inc/netflix-prize-data"""
 
 import os
+import subprocess
+from typing import Union
 import pandas as pd
 
 
-def load_data_part(
-    dataset_path: str,
-    part_file: str,
-    nrows: int | None = None
-) -> pd.DataFrame:
-    curr_movie_id = None
-    rows = []
-    with open(os.path.join(dataset_path, part_file), "r", encoding="utf-8") as f:
-        while nrows is None or len(rows) < nrows:
-            line = f.readline()
-            if not line:
-                break
+def download(destination: str) -> None:
+    """
+    Download and extract Netflix Prize dataset from
+    https://archive.org/download/nf_prize_dataset.tar/nf_prize_dataset.tar.gz.
+    """
+    destination = os.path.expanduser(destination)
+    os.makedirs(destination, exist_ok=True)
+    archive_path = os.path.join(destination, "nf_prize_dataset.tar.gz")
+    commands = [
+        f"curl -o {archive_path} -L https://archive.org/download/nf_prize_dataset.tar/nf_prize_dataset.tar.gz",
+        f"tar -xvzf {os.path.join(destination, 'download')} -C {destination}",
+        f"tar -xvzf {os.path.join(destination, 'download', 'training_set.tar')} -C {os.path.join(destination, 'download')}"
+    ]
+    for command in commands:
+        subprocess.call(command.split(" "))
+    os.remove(archive_path)
+    os.remove(os.path.join(destination, "download", "training_set.tar"))
 
-            line = line.strip()
-            if line.endswith(":"):
-                curr_movie_id = line[:-1]
-            else:
+def load_data(dataset_path: str, nrows: Union[int, None] = None) -> pd.DataFrame:
+    """
+    Load Netflix Prize training data.
+
+    Args:
+        dataset_path (str): Path to dataset directory.
+        nrows (int | None, optional): Number of rows to load. Defaults to
+        None.
+
+    Returns:
+        pd.DataFrame: Netflix Prize training data.
+    """
+    dataset_path = os.path.expanduser(dataset_path)
+    movies_path = os.path.join(dataset_path, "training_set")
+    movie_files = [f for f in os.listdir(movies_path) if f.endswith(".txt")]
+    movie_files = movie_files if nrows is None else movie_files[:nrows]
+    rows = []
+    for mf in movie_files:
+        with open(os.path.join(movies_path, mf), "r", encoding="utf-8") as f:
+            line = f.readline().strip()
+            movie_id = line.rstrip(":")
+            while True:
+                line = f.readline().strip()
+                if not line:
+                    break
+
                 user_id, rating, date = line.split(",")
                 row = {
-                    "movie_id": curr_movie_id,
+                    "movie_id": movie_id,
                     "user_id": user_id,
-                    "rating": rating,
+                    "rating": int(rating),
                     "date": date
                 }
                 rows.append(row)
     return pd.DataFrame(rows)
-
-def load_data(dataset_path: str) -> pd.DataFrame:
-    dataset_path = os.path.expanduser(dataset_path)
-    parts: list[pd.DataFrame] = []
-    for i in range(1, 5):
-        parts.append(load_data_part(dataset_path, f"combined_data_{i}.txt", nrows=100))
-    return pd.concat(parts, ignore_index=True)
